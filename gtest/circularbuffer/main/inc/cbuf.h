32cca83b9ff680a29ca07e03cd46b1adb60d9782
FlyingBBQ <derek.cardol@gmail.com>
2018-12-28 15:13:27 +0100

    adding googletest

---
+++ gtest/circularbuffer/main/inc/cbuf.h
@@ -0,0 +1,22 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* make the circular buffer an Abstract Data Type (ADT) */
+typedef struct
+circular_buffer {
+    uint8_t *buffer;
+    uint8_t *head;
+    uint8_t *tail;
+    size_t size;
+} cbuf_t;
+
+/* circular buffer functions */
+void cbuf_init(cbuf_t *cbuf, uint8_t *buffer, size_t size);
+int  cbuf_ptrpos(cbuf_t *cbuf, uint8_t *ptr);
+void cbuf_write(cbuf_t *cbuf, uint8_t data);
+uint8_t cbuf_read(cbuf_t *cbuf);
+
+#endif
---
+++ gtest/circularbuffer/main/src/cbuf.c
@@ -0,0 +1,53 @@
+#include "cbuf.h"
+#include <stdio.h>
+#include <assert.h>
+
+void
+cbuf_init(cbuf_t *cbuf, uint8_t *buffer, size_t size)
+{
+    cbuf->buffer = buffer;
+    cbuf->head = buffer;
+    cbuf->tail = buffer;
+    cbuf->size = size;
+}
+
+int
+cbuf_ptrpos(cbuf_t *cbuf, uint8_t *ptr)
+{
+    return (ptr - cbuf->buffer);
+}
+
+void
+cbuf_write(cbuf_t *cbuf, uint8_t data)
+{
+    if (cbuf_ptrpos(cbuf, cbuf->head) >= cbuf->size)
+    {
+        cbuf->head = cbuf->buffer;
+    }
+
+    *(cbuf->head++) = data;
+}
+
+uint8_t
+cbuf_read(cbuf_t *cbuf)
+{
+    int pos = cbuf_ptrpos(cbuf, cbuf->tail);
+
+    if (cbuf_ptrpos(cbuf, cbuf->tail) >= cbuf->size)
+    {
+        cbuf->tail = cbuf->buffer;
+    }
+
+    cbuf->tail++;
+
+    return (cbuf->buffer[pos]);
+}
+
+
+int
+main(void)
+{
+    printf("made\n");
+    return 0;
+}
+
